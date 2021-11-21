5ab9267d8a1f91bad4b488bd288c45bdc46b662e
Farya <76533997+Xfarya@users.noreply.github.com>
2021-11-21 11:36:36 +0000

    deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to peep_path(@peep)
   end
 
+  def destory 
+    @peep = Peep.find(params[:peep_id])
+    @comment = @peep.comments.find(params[:id])
+    @peep.destroy
+    redirect_to peep_path(@peep)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)

2a1a010783313cb5fe89f5f0c209d6427f5a2d8c
Farya <76533997+Xfarya@users.noreply.github.com>
2021-11-21 10:43:58 +0000

    added a peep new, create functionality

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -1,2 +1,3 @@
 class Peep < ApplicationRecord
+  validates :body, presence: true, length: { minimum: 1 } 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "peeps#index"
-  get "/peeps", to: "peeps#index"
-  get "/peeps/:id", to: "peeps#show"
+  resources :peeps
 end
